1880b2305e42d86eb1438a940a554e0f53d9b45a
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-13 11:42:51 +0200

    :star: add sort items feature

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,15 @@
+import Select from "react-select";
 import { ItemType } from "../lib/constants";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
+
+const sortingOptions = [
+  { value: "default", label: "Sort by default" },
+  { value: "packed", label: "Sort by packed" },
+  { value: "unpacked", label: "Sort by unpacked" },
+];
+
+type SortingOptionValue = (typeof sortingOptions)[number]["value"];
 
 export default function ItemList({
   items,
@@ -10,12 +20,35 @@ export default function ItemList({
   handleDeleteItem: (id: number) => void;
   handleToggleItem: (id: number) => void;
 }) {
+  const [sortBy, setSortBy] = useState<SortingOptionValue>("default");
+  const sortedItems = [...items].sort((a: ItemType, b: ItemType) => {
+    if (sortBy === "packed") {
+      return Number(b.packed) - Number(a.packed);
+    }
+    if (sortBy === "unpacked") {
+      return Number(a.packed) - Number(b.packed);
+    }
+    return a.id - b.id;
+  });
+
   return (
     <>
       <ul className="item-list">
         {!items.length && <EmptyView />}
 
-        {items.map((item) => (
+        {items.length && (
+          <section className="sorting">
+            <Select
+              onChange={(option) => {
+                if (option) setSortBy(option.value);
+              }}
+              options={sortingOptions}
+              defaultValue={sortingOptions[0]}
+            />
+          </section>
+        )}
+
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
