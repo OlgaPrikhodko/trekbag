5b761c216723e1ffbf303692c599783528c437c7
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-13 05:17:55 +0200

    :star: render initialItems

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,3 +1,26 @@
+const initialItems = [
+  { name: "good mood", packed: true },
+  { name: "passport", packed: true },
+  { name: "phone charger", packed: true },
+];
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {initialItems.map((item) => (
+        <Item item={item} key={item.name} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }: { item: { name: string; packed: boolean } }) {
+  return (
+    <li className="item">
+      <label htmlFor="999">
+        <input type="checkbox" value={item.packed} />
+        {item.name}
+      </label>
+    </li>
+  );
 }
